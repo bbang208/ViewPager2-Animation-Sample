0552afac1f84b8ac671182c3f6c74da30f7c15fc
bbang208 <bbang@coinplug.com>
2021-08-30 11:24:14 +0900

    Preview Next Page

--- app/src/main/java/io/github/bbang208/diffviewpager/ZoomOutPageTransformer.kt
+++ app/src/main/java/io/github/bbang208/diffviewpager/ZoomOutPageTransformer.kt
@@ -2,6 +2,7 @@ package io.github.bbang208.diffviewpager
 
 import android.view.View
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.card.MaterialCardView
 
 private const val MIN_SCALE = 0.85f
 private const val MIN_ALPHA = 0.5f
@@ -9,6 +10,11 @@ private const val MIN_ALPHA = 0.5f
 class ZoomOutPageTransformer : ViewPager2.PageTransformer {
 
     override fun transformPage(view: View, position: Float) {
+        val screenWidth = view.resources.displayMetrics.widthPixels
+        val pageMarginPx = 36
+        val pagerWidth = view.findViewById<MaterialCardView>(R.id.certificationCard).measuredWidth
+        val offsetPx = screenWidth - pageMarginPx - pagerWidth
+
         view.apply {
             val pageWidth = width
             val pageHeight = height
@@ -22,12 +28,13 @@ class ZoomOutPageTransformer : ViewPager2.PageTransformer {
                     val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
                     val vertMargin = pageHeight * (1 - scaleFactor) / 2
                     val horzMargin = pageWidth * (1 - scaleFactor) / 2
-                    translationX = if (position < 0) {
-                        horzMargin - vertMargin / 2
-                    } else {
-                        horzMargin + vertMargin / 2
-                    }
+//                    translationX = if (position < 0) {
+//                        horzMargin - vertMargin / 2
+//                    } else {
+//                        horzMargin + vertMargin / 2
+//                    }
                     // Scale the page down (between MIN_SCALE and 1)
+                    translationX = position * -offsetPx
                     scaleX = scaleFactor
                     scaleY = scaleFactor
 
